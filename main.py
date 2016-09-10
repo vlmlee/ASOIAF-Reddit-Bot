1b27fce111131b99db78b0ff5ec792ad484b92e2
Michael Lee <mlee@vellichorlabs.com>
2016-09-10 02:43:30 -0800

    Added a spell checker written by Peter Norvig at http://norvig.com/spell-correct.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw
 import re
 import json
+import spell
 from time import sleep
 from collections import deque
 
@@ -14,13 +15,23 @@ def mispelled(c):
 	words_to_spellcheck = [word for word in tokens if word.startswith(letters) and len(word) > 2 and len(word) < 12]
 
 	for item in words_to_spellcheck:
-		if some_condition:
+		if check_if_named_weapon(item):
+			return True
+
+def check_if_named_weapon(word):
+	if not correction(word):
+		# if word is spelled correctly
+		return False
+	else
+		# if word is a slight mispelling
+		if condition: # check if the mispelled word matches any of the weapons
 			return True
 
 def correct_spelling(c, verbose=True, respond=False):
 	# find corrected word
 	# tell user that it has been corrected
 	# reply with description
+	correction = "Returning the corrected word and description"
 
 	if verbose:
 		print c.body.encode('UTF-8')
@@ -33,7 +44,7 @@ def correct_spelling(c, verbose=True, respond=False):
 
 
 if __name__ == '__main__':
-	cache = queue(maxlen=200)
+	cache = deque(maxlen=200)
 	r = praw.Reddit(user_agent = user_agent)
 	r.login(REDDIT_USERNAME, REDDIT_PASS)
 
