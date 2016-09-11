ce0224df501aa82c1b4a539d786d85cea295281f
Michael Lee <mlee@vellichorlabs.com>
2016-09-11 05:47:27 -0800

    Removed pesky compiled python files

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
-user_agent = ("ASOIAF Named Weapons Spell Checker Bot v0.1")
+user_agent = ("ASOIAF Named Weapons Spell Checker Bot v0.2")
 REDDIT_USERNAME = os.environ['REDDIT_USERNAME']
 REDDIT_PASS = os.environ['REDDIT_PASS']
